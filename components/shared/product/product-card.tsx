75ce4316f0050146eab20dc18714ca84de01c2af
ravichandrapalla <cravi6635@gmail.com>
2025-05-28 12:49:29 +0530

    fixed price compoent in card

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import ProductPrice from "./product-price";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
@@ -25,7 +26,7 @@ const ProductCard = ({ product }: { product: any }) => {
         <div className="flex-between gap-4">
           <p>{product.rating} Stars</p>
           {product.stock > 0 ? (
-            <p className="font-bold">${product.price}</p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
             <p className="text-destructive">Out of stock</p>
           )}
