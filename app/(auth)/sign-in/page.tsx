6985c6739b882930025e79fb5287c42416231a44
ravichandrapalla <cravi6635@gmail.com>
2025-05-30 16:26:37 +0530

    added signin ui

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,44 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import CredentialsSigninForm from "./credentials-signin-form";
+
+export const metadata: Metadata = {
+  title: "Sign-in",
+};
+
+const SignInPage = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader>
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              alt="main logo"
+              width={100}
+              height={100}
+              priority
+            />
+          </Link>
+          <CardTitle className="text-center">Sign-in</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <CredentialsSigninForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInPage;
